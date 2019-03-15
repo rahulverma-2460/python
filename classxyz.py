7538763c5225306dae496b27a0bae3af2bdd2a9f
rahulverma-2460 <48345213+rahulverma-2460@users.noreply.github.com>
2019-03-15 12:28:45 +0530

    Add files via upload

---
+++ classxyz.py
@@ -0,0 +1,12 @@
+class time:
+       def __init__(self):
+         self.time=0
+       def gettime(self):
+          self.time=input("enter the time")
+       def showtime(self):
+           print(self.time)
+o1=time()
+o1.gettime()
+o1.showtime()
+           
+    
